3c0d5566104383860f6bc02841af21359005537a
JamesHughes <jhughes1790@gmail.com>
2022-04-11 13:31:44 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import HeartsList from './components/HeartsList';
+import Header from './components/Header';
+import './App.css';
+
+
+
+
+
+
+function App() {
+  return (
+    <>
+        <Header />
+        <HeartsList/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+const Header = (props) => {
+    return (
+      <div className="App-header">I ❤️ React</div>
+    )
+  }
+
+
+
+export default Header;
